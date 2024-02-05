d561c69df15358ffbaa4ff04bef61c3a3303eda0
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-02-05 12:30:31 -0500

    Create test for animation

---
+++ test/animation_screen_test.dart
@@ -0,0 +1,38 @@
+import 'package:animation_widget_test/animation_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'Animation screen test',
+    (tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: AnimationScreen(),
+        ),
+      );
+
+      var containerFinder = find.byType(Container);
+      expect(containerFinder, findsOneWidget);
+      var container = tester.widget<Container>(containerFinder);
+
+
+      // Initial 
+      expect(container.constraints!.minWidth, 50);
+      expect(container.constraints!.minHeight, 50);
+      expect((container.decoration as BoxDecoration).color, Colors.blue);
+      expect((container.decoration as BoxDecoration).borderRadius, BorderRadius.zero);
+
+      await tester.pumpAndSettle();
+      containerFinder = find.byType(Container);
+      container = tester.widget<Container>(containerFinder);
+
+      // End of animation
+      expect(container.constraints!.minWidth, 200);
+      expect(container.constraints!.minHeight, 200);
+      expect((container.decoration as BoxDecoration).color, Colors.green);
+      expect((container.decoration as BoxDecoration).borderRadius, BorderRadius.circular(50));
+
+    },
+  );
+}
